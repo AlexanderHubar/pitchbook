e9ca5a194b3201fa62d2ab24633e0935e6434fae
Olexandr Hubar <alexandr.hubar1999@gmail.com>
2019-05-28 10:02:37 +0300

    header adaptivity for 425px

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 const tabsContainer = document.getElementById('tab1');
+    
+const wrapperMenu = document.querySelector('.wrapper-menu');
+const mobileNav = document.querySelector('.navigation-mobile');
+wrapperMenu.addEventListener('click',() => {
+	wrapperMenu.classList.toggle('open'); 
+	mobileNav.classList.toggle('visible');
+});
 
 tabChange(tabsContainer);
